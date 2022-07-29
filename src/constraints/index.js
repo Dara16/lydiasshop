6d10a8d5676509f66133930eca8f3f92eae7a3d1
Dara16 <just4oludee@yahoo.com>
2022-07-29 19:47:31 -0400

    corrected base url

--- src/constraints/index.js
+++ src/constraints/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://hidden-ridge-91641.herokuapp.com/'
+export const BASE_URL = 'https://hidden-ridge-91641.herokuapp.com/'
 //export const BASE_URL = 'http://localhost:3000/'
