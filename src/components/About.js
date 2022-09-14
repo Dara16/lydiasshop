846ae2aabcabdadfe2ad97cf522daa808fcfff75
Dara16 <just4oludee@yahoo.com>
2022-09-14 09:12:22 -0400

    typo corrected in about

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div>
             <h3 className="about">
-                Our cozy, little bakeshop is named after our Grandmother Lydia who was an avid baker. She treated her entire family to her delightful recipes
+                Our cozy little bakeshop is named after our Grandmother Lydia who was an avid baker. She treated her entire family to her delightful recipes
                 over decades of family events. We are proud to continue her legacy by sharing with the world her carefully handcrafted recipes, which were passed down to us. 
                 We hope you enjoy the treats as much as we always have!
             </h3>
